28c457013a7229df5bb9c2b25e17263bd9b880d2
Matheus Gaudencio <matheusgr@gmail.com>
2020-03-03 16:00:37 -0300

    Add prompt test.

--- tutorterminal/tests/test_commands.py
+++ tutorterminal/tests/test_commands.py
@@ -6,6 +6,10 @@ import tutorterminal
 
 
 class TestCommands(TestCase):
+
+    def patch(self, results_list):
+        tutorterminal.ansiprint = lambda x, end=None: results_list.append(x)
+
     def test_checkfile(self):
         self.assertTrue(tutorterminal.checkfile(sys.argv[0]))
 
@@ -17,3 +21,9 @@ class TestCommands(TestCase):
 
     def test_run_command_auto_fail(self):
         self.assertFalse(tutorterminal.run_command("dir __1234__", auto=True))
+
+    def test_prompt(self):
+        results = []
+        self.patch(results)
+        tutorterminal.prompt()
+        self.assertRegex(results[0], '.*\\$ $')
