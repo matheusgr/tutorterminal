202b64e148a2b324c25dea34636aee4c263b3ed2
Matheus Gaudencio <matheusgr@gmail.com>
2020-03-12 13:06:38 -0300

    Setting download_url to use master tallball and preparing other fields to publish.

--- setup.py
+++ setup.py
@@ -4,22 +4,27 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="tutorterminal-matheusgr",
-    version="0.0.1",
+    name="tutorterminal",
+    version="v0.1",
     scripts=["ttterminal"],
     author="Matheus Gaudencio",
     author_email="matheusgr@gmail.com",
-    description="A small example package",  # TODO
+    description="A tool to show terminal tutorials.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/matheusgr/tutorterimanl",
+    url="https://github.com/matheusgr/tutorterminal",
+    download_url='https://github.com/matheusgr/tutorterminal/tarball/master',
     packages=setuptools.find_packages(),
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Education :: Computer Aided Instruction (CAI)",
+        "Topic :: System :: Shells",
+        "Intended Audience :: Education",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.2',
     test_suite='nose.collector',
     tests_require=['nose'],
 )
